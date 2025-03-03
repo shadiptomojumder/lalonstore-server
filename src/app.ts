4617e22c8656e4f45502f200ec7f52ca4376a8f8
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-03-04 01:53:51 +0600

    Change rateLimit "max" 100 to 1000

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   })
 );
 
